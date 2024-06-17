385ae90e0161c3036345cf37b793bf2b450062b8
lux <luxofons@gmail.com>
2024-06-17 08:46:14 +0700

    fix path

--- identity-svc/src/main/java/com/quyennv/identitysvc/controller/UserController.java
+++ identity-svc/src/main/java/com/quyennv/identitysvc/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
 public class UserController {
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
